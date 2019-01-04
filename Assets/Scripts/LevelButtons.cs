4a4804f5b9eaf3a9b3f41e57d31d4027491e9aae
Colin Cop <colincop@ucm.es>
2019-01-04 21:42:42 +0100

    levelselect cimientos

---
+++ Assets/Scripts/LevelButtons.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelButtons : MonoBehaviour {
+
+    // Use this for initialization
+    int _id;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void SetId(int id)
+    {
+        _id = id;
+    }
+    public int GetId()
+    {
+        return _id;
+    }
+    public void SelectLevel()
+    {
+      //  Destroy(this);
+       Debug.Log("Hola soy el " + _id);
+    }
+}
---
+++ Assets/Scripts/PopulateGrid.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulateGrid : MonoBehaviour
+{
+
+    // Use this for initialization
+    public GameObject boton;
+    public int numNiveles;
+    void Start()
+    {
+        Create();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void Create()
+    {
+        GameObject aux;
+        for (int i = 0; i < numNiveles; i++)
+        {
+            aux = (GameObject)Instantiate(boton, transform);
+            aux.GetComponent<LevelButtons>().SetId(i);
+        }
+    }
+}
